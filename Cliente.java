20685c486934b9fdf6c40f74c9015acd1d9c8869
SAMUEL GOMES MONNI <2023326456@ALUNOS.OSORIO.IFRS.EDU.BR>
2025-08-28 16:35:29 -0300

    Classe cliente feita

--- Cliente.java
+++ Cliente.java
@@ -1,6 +1,58 @@
 public class Cliente {
-    public static void main(String[] args) {
-        
-    }
 
+    private String nome;
+    private String cpf;
+    private String endereco;
+    private String telefone;
+    private String email;
+    private String dataNascimento;
+
+    public Cliente(String nome, String cpf, String endereco, String telefone, String email, String dataNascimento) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.endereco = endereco;
+        this.telefone = telefone;
+        this.email = email;
+        this.dataNascimento = dataNascimento;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    public String getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+    public String toString() {
+        return "Cliente [nome=" + nome + ", cpf=" + cpf + ", endereco=" + endereco + ", telefone=" + telefone
+                + ", email=" + email + ", dataNascimento=" + dataNascimento + "]";
+    }
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Cliente cliente1 = new Cliente("Samuel", "12345678901", "casa01", "190", "emailSamuel", "17/12/2007");
+        Scanner scanner = new Scanner(System.in);
+
+        int resposta = 0;
+  
+
         
     }
 
